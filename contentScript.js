2ee72e86edaa93f1b43f8ef097e47468abc1e41c
yukari-n-erb <45537257+yukari-n-erb@users.noreply.github.com>
2023-04-04 14:15:15 +0900

    remove unused code

--- contentScript.js
+++ contentScript.js
@@ -1,17 +1,17 @@
 function observeDOMChanges() {
     // Get a target element
     const targetNode = document.body;
-  
+
     // Set observer
     const config = { childList: true, subtree: true };
-  
+
     // Callback function when new Chat page access
     const callback = (mutationsList) => {
       for (const mutation of mutationsList) {
         if (mutation.type === "childList") {
           const currentUrl = window.location.href;
           const targetUrl = 'https://chat.openai.com/chat';
-  
+
           if (currentUrl === targetUrl) {
             // Redirect to GPT-4 chat page
             const redirectToUrl = 'https://chat.openai.com/chat?model=gpt-4';
@@ -20,14 +20,14 @@ function observeDOMChanges() {
         }
       }
     };
-  
+
     // Create an observer instance
     const observer = new MutationObserver(callback);
-  
+
     // Start observing to target element
     observer.observe(targetNode, config);
   }
-  
+
   // Start observing DOM changes
   observeDOMChanges();
   
